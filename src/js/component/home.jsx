43f3c49977d36c90911f598b0635c652f66235ab
jcabrera53 <juancabrera53@gmail.com>
2022-11-26 22:57:20 +0000

    primera version lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,17 +2,28 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	/*const [light, setLight] = useState(); */
+	const red_on ="red-on";
+	const yellow_on ="yellow-on";
+	const green_on ="green-on";
+	const red_off ="red-off";
+	const yellow_off ="yellow-off";
+	const green_off ="green-off";
+
+	const [lightRed, setLightRed] = useState(red_off);
+	const [lightYellow, setLightYellow] = useState(yellow_off);
+	const [lightGreen, setLightGreen] = useState(green_off);
+
 	return (
 		<div className="main-content">
 		<h1 className="text-center mt-5">The Traffic Light</h1>
 			<div className="cuerpo">
-				<div className="red"></div>
-				<div className="yellow"></div>
-				<div className="green"></div>				
+				<div className={lightRed} onClick={()=>setLightRed(red_on) + setLightYellow(yellow_off) + setLightGreen(green_off)}></div>
+				<div className={lightYellow} onClick={()=>setLightYellow(yellow_on) + setLightRed(red_off) + setLightGreen(green_off)}></div>
+				<div className={lightGreen} onClick={()=>setLightGreen(green_on) + setLightYellow(yellow_off) + setLightRed(red_off)}></div>				
 			</div>
 			</div>
 	);
